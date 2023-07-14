239c1789fca39db823c68df8a06a4e56af7e3500
Brenno Cavalcante <brennofaby@gmail.com>
2023-07-13 22:55:50 -0300

    feat: added static config and skill module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,18 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { GameModule } from './game/game.module';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
+import { SkillModule } from './skill/skill.module';
 
 @Module({
-  imports: [ConfigModule.forRoot(), GameModule],
+  imports: [
+    ConfigModule.forRoot(),
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'public'),
+    }),
+    GameModule,
+    SkillModule,
+  ],
 })
 export class AppModule {}
