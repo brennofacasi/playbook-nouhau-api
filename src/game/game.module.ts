8c0f0f4bdd800b63cc1334073a0ebb73eeea1dc5
Brenno Cavalcante <brennofaby@gmail.com>
2023-07-13 22:54:52 -0300

    feat: added prisma service

--- src/game/game.module.ts
+++ src/game/game.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { GameService } from './game.service';
 import { GameController } from './game.controller';
+import { PrismaService } from 'src/database/prisma.service';
 
 @Module({
-  providers: [GameService],
-  controllers: [GameController]
+  providers: [GameService, PrismaService],
+  controllers: [GameController],
 })
 export class GameModule {}
