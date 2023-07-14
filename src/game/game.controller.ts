9ab59da5dac0cbfa7d463ab1b24fa2ff119838d5
Brenno Cavalcante <brennofaby@gmail.com>
2023-07-13 22:54:35 -0300

    feat: added get and patch routes

--- src/game/game.controller.ts
+++ src/game/game.controller.ts
@@ -1,4 +1,31 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Param,
+  Patch,
+  Query,
+  Req,
+} from '@nestjs/common';
+import { Request } from 'express';
+import { GameService } from './game.service';
 
 @Controller('game')
-export class GameController {}
+export class GameController {
+  constructor(private readonly gameService: GameService) {}
+
+  @Get()
+  getGames(@Query() query: any, @Req() req: Request) {
+    return this.gameService.getGames(query, req);
+  }
+
+  @Get(':id')
+  getGame(@Param('id') id: number, @Req() req: Request) {
+    return this.gameService.getGame(id, req);
+  }
+
+  @Patch(':id')
+  updateGame(@Param('id') id: number, @Body() body: any) {
+    return this.gameService.updateGame(id, body);
+  }
+}
